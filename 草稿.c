f68f47d661d3d6ef51f9d667fba81cac142b448d
user-xxl <117566016+user-xxl@users.noreply.github.com>
2022-11-14 22:39:47 +0800

    Add files via upload

--- 草稿.c
+++ 草稿.c
@@ -568,3 +568,23 @@
 //	out:
 //	return 0;
 //}
+
+//不同数据类型大小的计算
+#include<stdio.h>
+int main()
+{
+	int a;
+	a = 6;
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(a));
+	//sizeof(a++) 在程序运行时整个的sizeof(a++)就被替换为a++所代表的数据类型的值，因此后续输出a的值也不会变
+	return 0;
+}
+//% d：用于 int
+//% ld：用于 long
+//% lld：用于 longlong
+//输入时：
+//float 输入用% f
+//double 输入用% lf
+//输出时：
+//float, double都用% f 输出就行了(不太清楚是什么环境下，double 用% lf 会出错)
